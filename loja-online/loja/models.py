11d05430a5540d1813f64218f49249b00d7e95e8
Rui Costa <rui.costa@meu.ipta.pt>
2026-04-02 11:25:22 +0100

    criacao de modelos

--- loja-online/loja/models.py
+++ loja-online/loja/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Vendedor(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.CharField(max_length=200)
+    data_de_criacao = models.DateTimeField("Data de incrição")
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.CharField(max_length=200)
+
+class Artigo(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.CharField(max_length=200)
+    preco = models.CharField(max_length=200)
+    data_de_criacao = models.DateTimeField("Data de publicação")
+    vendedor = models.ForeignKey(Vendedor, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
