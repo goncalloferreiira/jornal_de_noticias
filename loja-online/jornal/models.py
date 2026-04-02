b022e5ee6eddc29d661d1889316b09bd2c8ea985
MiguelAP6 <miguel.paranhos.2025073@my.istec.pt>
2026-04-03 00:03:31 +0100

    configuração dos modelos

--- loja-online/jornal/models.py
+++ loja-online/jornal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    corpo = models.TextField()
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    artigo = models.ForeignKey(Artigo, on_delete=models.CASCADE, related_name='comentarios')
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comentário no artigo: {self.artigo.titulo}'
