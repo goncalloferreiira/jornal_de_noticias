4a495395ff8c9bd4ffe010d5cad6b9219662425e
mayy <goncalo752006@gmail.com>
2026-04-02 21:51:45 +0100

    limpeza do projeto base, remocao da app loja e criacao da app jornal

--- loja-online/website/settings.py
+++ loja-online/website/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'loja.apps.LojaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
