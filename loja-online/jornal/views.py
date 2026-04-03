5536732733f78230e6c9832860150fa96c6ef539
MiguelAP6 <miguel.paranhos.2025073@my.istec.pt>
2026-04-03 18:58:31 +0100

    novo visual e pagina de detalhe

--- loja-online/jornal/views.py
+++ loja-online/jornal/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from .models import Artigo # Importa os teus modelos
+from django.shortcuts import render, get_object_or_404
+from .models import Artigo
 
 def home(request):
-    # Vai buscar todos os artigos da base de dados
     artigos = Artigo.objects.all().order_by('-data_publicacao') 
     return render(request, 'jornal/home.html', {'artigos': artigos})
+
+def detalhe(request, id):
+    artigo = get_object_or_404(Artigo, id=id)
+    return render(request, 'jornal/detalhe.html', {'artigo': artigo})
