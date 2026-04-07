fb09cc65ea48ac10edd44777c86dae067969f6a3
mayy <goncalo752006@gmail.com>
2026-04-07 18:39:52 +0100

    implementar sistema de comentarios e pagina de visualizacao

--- loja-online/jornal/urls.py
+++ loja-online/jornal/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('noticia/<int:id>/', views.detalhe, name='detalhe'),
+    path('noticia/<int:id>/comentarios/', views.comentarios, name='comentarios'),
 ]
--- loja-online/jornal/views.py
+++ loja-online/jornal/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Artigo
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Artigo, Comentario 
 
 def home(request):
     artigos = Artigo.objects.all().order_by('-data_publicacao') 
@@ -8,3 +8,19 @@ def home(request):
 def detalhe(request, id):
     artigo = get_object_or_404(Artigo, id=id)
     return render(request, 'jornal/detalhe.html', {'artigo': artigo})
+
+def comentarios(request, id):
+    artigo = get_object_or_404(Artigo, id=id)
+
+    if request.method == 'POST':
+        texto_novo = request.POST.get('texto')
+        if texto_novo:
+            Comentario.objects.create(artigo=artigo, texto=texto_novo)
+            return redirect('comentarios', id=artigo.id)
+        
+    lista_comentarios = artigo.comentarios.all().order_by('-data_criacao')
+        
+    return render(request, 'jornal/comentarios.html', {
+        'artigo': artigo,
+        'comentarios': lista_comentarios
+    })
